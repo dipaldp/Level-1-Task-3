f73bec6f5632a94c21a1e9f69cf4a720b9d924ee
dipaldp <4udipalpatel@gmail.com>
2024-05-21 13:19:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let displayValue='0';
+
+function appendToDisplay(value){
+    if(displayValue=='0' && value!=='.'){
+        displayValue = value;
+    }
+    else{
+        displayValue += value;
+    }
+    document.getElementById('display').innerText = displayValue;
+}
+
+function clearDisplay(){
+    displayValue='0';
+    document.getElementById('display').innerText = displayValue;
+}
+
+function calculate(){
+    try{
+        displayValue = eval(displayValue).toString();
+        document.getElementById('display').innerText = displayValue;
+    }catch(error){
+        displayValue = 'Error';
+        document.getElementById('display').innerText = displayValue;
+    }
+}
